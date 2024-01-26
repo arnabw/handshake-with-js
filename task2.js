f97ea3a2f1c65bf60669bf0d0ee9c8918cd55c2b
arnabw <arnabsahawrk@gmail.com>
2024-01-26 13:01:47 +0600

    completed task2

---
+++ task2.js
@@ -0,0 +1,10 @@
+var math = 75.25;
+var bio = 65;
+var chem = 80;
+var phy = 35.45;
+var ban = 99.50;
+
+var totalResult = math + bio + chem + phy + ban;
+var avg = totalResult / 5;
+
+console.log(avg.toFixed(2));
